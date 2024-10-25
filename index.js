771a3248f5caf53e367ca9285b34458e6eca4e8d
soydat <francoalmeida400@gmail.com>
2024-10-25 15:40:24 -0300

    exercise done

--- index.js
+++ index.js
@@ -25,6 +25,37 @@ const cosasQueAprendimos = [
   },
 ];
 
-function main() {}
+function rmvli (){
+  const listaEl = document.getElementsByClassName("lista")[0]; // Accedemos al primer elemento de la colección
+  while (listaEl.childNodes.length > 0) { // Verificamos que haya nodos hijos
+    console.log("rmvli() executed");
+    const liEl = listaEl.childNodes[0]; // Siempre eliminamos el primer nodo
+    liEl.remove();
+  }
+}
+
+function createli () {
+  const listaEl = document.getElementsByClassName("lista")[0];
+  console.log("createli() executed");
+
+  for (let i = 0; i < cosasQueAprendimos.length; i++) {
+
+    const element = cosasQueAprendimos[i];
+    const newLiEl = document.createElement("li");
+
+    newLiEl.innerText = element.tema
+    if (element.class){
+      newLiEl.classList.add(element.class);
+    }
+
+    listaEl.appendChild(newLiEl);
+  }
+}
+
+function main() {
+    rmvli();
+    createli();
+}
+
 
 main();
